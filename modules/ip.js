fb4cdf4c3f70698c01309d14d2ab57e4c3ad8180
helong.gao <helong.gao@sinobbd.com>
2018-06-05 17:30:40 +0800

    koa2: mysql

--- modules/ip.js
+++ modules/ip.js
@@ -2,6 +2,8 @@ let query = require('../lib/mysql');
 
 // 获取ip列表.
 exports.ipList = () => {
-    let sql = `SELECT * FROM ip ORDER BY id DESC LIMIT 10;`;
-    return query(sql);
+    // let values = [`' and 1=1`]; // 注入.
+    let values = [6];
+    let sql = `SELECT * FROM ip ORDER BY id DESC LIMIT ?;`;
+    return query(sql, values);
 };
